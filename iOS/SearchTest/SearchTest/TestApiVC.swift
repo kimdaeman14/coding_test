1d4d83d4255e42d73b9f5e8fe8b79577fce9be1a
kimdaeman14 <kimdaeman14@gmail.com>
2018-09-12 15:17:44 +0900

    네트워킹 구조변경 Alamofire->URLSession

--- iOS/SearchTest/SearchTest/TestApiVC.swift
+++ iOS/SearchTest/SearchTest/TestApiVC.swift
@@ -7,59 +7,37 @@
 //
 
 import UIKit
-import Alamofire
-
 
 class TestApiVC: UIViewController {
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        takeData()
-        
+        makePostCall()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func takeData(){
-        
-        let url = URL(string: "https://en.wikipedia.org/w/api.php")
-        
-        Alamofire.request(url!, method: HTTPMethod.get, parameters:["action":"opensearch","search":"purple","limit": 20,"namespace":0,"format":"json"])
-            .validate(statusCode: 200..<400)
-            .responseData { [weak self](response) in
-                guard let strongSelf = self else { return }
-                switch response.result{
-                case .success(let value):
-                    print(value, "일단 성공")
-                    do{
-                        print("1")
-                        
-                        let apiData = try JSONSerialization.jsonObject(with: value, options: [])
-                        print(apiData)
-                        let first = apiData as! NSArray
-                        print(first[0])
-                        print(first[1])
-                        print(first[2])
-                        
-//                       let apiData = try JSONDecoder().decode([Search].self, from: value)
-//                        print(apiData)
-//                        let apiData = try JSONDecoder().decode(Cart.self, from: value)
-//                        print(apiData)
-                        print("2")
-                    }catch{
-                        print(error.localizedDescription, "success")
-                    }
-                case .failure(let error):
-                    print(error, "failure")
-                }
-        }.resume()
+    func makePostCall() {
+        let todosEndpoint = "https://en.wikipedia.org/w/api.php?action=opensearch&search=purple"
+        guard let todosURL = URL(string: todosEndpoint) else {
+            return print("검색값이 잘못되었습니다.")
+        }
+        let todosUrlRequest = URLRequest(url: todosURL)
+        let task = URLSession.shared.dataTask(with: todosUrlRequest) { (data, response, error) in
+            guard error == nil else { return print(error!) }
+            guard let data = data else { return print("data가 없습니다.") }
+            do {
+                let receivedTodo = try JSONSerialization.jsonObject(with: data, options: [])
+                let first = receivedTodo as! NSArray
+                print(first[0])
+                print(first[1])
+                print(first[2])
+            } catch  {
+                print("parsing response가 잘못되었습니다.")
+                return
+            }
+        }
+        task.resume()
     }
 
-   
 
 }
